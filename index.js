b83e3b92cc214e68b38ad114699117a558999a5d
Julian Pufler <puf17640@spengergasse.at>
2020-05-18 14:38:23 +0200

    added eslint config

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const CentraRequest = require('./lib/CentraRequest')
 
 module.exports = {
-  fetch: (url, method) => new CentraRequest(url, method),
-  get: (url) => new CentraRequest(url),
-  post: (url, data, contentType = 'json') => new CentraRequest(url, 'POST').body(data, contentType),
-  put: (url, data, contentType = 'json') => new CentraRequest(url, 'PUT').body(data, contentType),
-  delete: (url, data, contentType = 'json') => new CentraRequest(url, 'DELETE').body(data, contentType),
+	fetch: (url, method) => new CentraRequest(url, method),
+	get: (url) => new CentraRequest(url),
+	post: (url, data, contentType = 'json') => new CentraRequest(url, 'POST').body(data, contentType),
+	put: (url, data, contentType = 'json') => new CentraRequest(url, 'PUT').body(data, contentType),
+	delete: (url, data, contentType = 'json') => new CentraRequest(url, 'DELETE').body(data, contentType)
 }
